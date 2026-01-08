44cb02dcb5f994fee5d5612a26b635a1d960c508
DevOpsJeremy <DevOpsJeremy@gmail.com>
2026-01-08 23:48:56 +0000

    Updates

---
+++ src/tize/config.py
@@ -0,0 +1,43 @@
+def get_file_config(path: Path) -> dict:
+    if not path.is_file():
+        raise Exception(f"Invalid path: {path}. NOT a file.")
+    
+    config = default_config.copy()
+    comment_regex = r"^(\#-{3,}scaffolding\n(?P<scaffold_config>(\#.*\n)*)\#-{3,}\n?)?(?P<content>(.*\n?)*)"
+    if path.suffix in [".md", ".markdown", ".html"]:
+        comment_regex = r"^(<!\-\-( *\n)*-{3,}scaffolding\n(?P<scaffold_config>(.*\n)*)-{3,}\n?(.*\n)*\-\->)?(?P<content>(.*\n?)*)"
+    pattern = re.compile(
+        comment_regex,
+        re.MULTILINE
+    )
+    file_content = path.read_text()
+    matches = pattern.search(file_content)
+    print(f"Getting file config for: {path}")
+    print(matches)
+    print(matches.groupdict())
+    config_text_raw = matches.group("scaffold_config")
+    if not config_text_raw:
+        print("No config text found, returning default config")
+        return config
+    config_text = config_text_raw
+    if path.suffix not in [".md", ".markdown", ".html"]:
+        config_text = re.sub(r"^\#", "", config_text_raw, flags=re.MULTILINE)
+    print(f"Config text:\n{config_text}")
+    config_text_cleaned = textwrap.dedent(config_text).strip()
+    print(f"Config text cleaned:\n{config_text_cleaned}")
+    try:
+        file_config = yaml.safe_load(config_text_cleaned)
+        print(f"Config loaded:\n{file_config}")
+        merge_dicts(config, file_config)
+        print(f"Config merged:\n{config}")
+    except Exception as e:
+        raise e
+    return config
+
+def merge_dicts(a: dict, b: dict):
+    for key in b:
+        if key in a and isinstance(a[key], dict) and isinstance(b[key], dict):
+            merge_dicts(a[key], b[key])
+        else:
+            a[key] = b[key]
+    return a
